fcf591417e0f7b3ebd91838c5f59de8fd3c870b0
inada tsukasa <invincibility3@icloud.com>
2022-04-29 21:17:19 +0900

    inada is_placable 改善

--- board.py
+++ board.py
@@ -1,23 +1,57 @@
 import numpy as np
 
 
-class StepGenerator:
-    def __init__(self, limit, startpoint, step):
-        self.count = 0
-        self.limit = limit
+# 下のジェネレータの引数となる (step, num) を８方向分生成するジェネレータ
+class StepNumGenerator:
+    def __init__(self, startpoint, width, height):
+        up, left = divmod(startpoint, width)
+        right = width - 1 - left
+        down = height - 1 - up
+
+        self.nums = [up, right, down, left]
+        self.width = width
+
+    # 引数には正負の符号を指定する
+    def __gen(self, sign):
+        # 水平方向探索用の (step, num)
+        yield sign, self.nums[sign]
+
+        step_base = sign * self.width
+        num_base = self.nums[sign + 1]
+
+        for bias in range(-1, 2):
+            if bias:
+                # 斜め方向探索用の (step, num)
+                yield step_base + bias, min(num_base, self.nums[bias])
+            else:
+                # 垂直方向探索用の (step, num)
+                yield step_base, num_base
+
+    @property
+    def generator(self):
+        yield from self.__gen(1)
+        yield from self.__gen(-1)
+
+
+# オセロ盤の各位置から８方向それぞれに、存在する石を探索するときに使うジェネレータ
+class ElementNumRange:
+    def __init__(self, startpoint, step, num):
         self.value = startpoint
         self.step = step
+        self.count = 0
+        self.num = num
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.count >= self.limit:
+        if self.count >= self.num:
             raise StopIteration()
 
         self.count += 1
         self.value += self.step
         return self.value
+
 
 
 
@@ -27,9 +61,11 @@ class Board:
         self.stone_black = 0
         self.turn = 1
 
-        assert not self.height % 2 and self.height < 9
-        assert not self.width % 2 and self.width < 9
+        assert not self.height % 2
+        assert not self.width % 2
+        assert self.action_size <= 64
 
+    # property でデコレートすることでメソッドを属性のようにして呼び出すことができるようになる
     @property
     def height(self):
         return 8
@@ -46,9 +82,11 @@ class Board:
         return np.array([self.stone_exist, self.stone_black])
 
 
+    # オセロ盤の各位置を表す、行列のインデックス (tuple) を通し番号に変換するためのメソッド
     def t2n(self, t):
         return self.width * t[0] + t[1]
 
+    # オセロ盤を初期状態にセットするためのメソッド
     def reset(self):
         shift = self.t2n((self.height // 2 - 1, self.width // 2 - 1))
         self.stone_exist = (0b11 << shift) + (0b11 << (shift + self.width))
@@ -56,6 +94,7 @@ class Board:
         self.turn = 1
 
 
+    # 指定された 64 bit 整数の下から n bit 目の値を取得するためのメソッド
     def __getbit(self, x, n):
         x = x >> n
         return x & 1
@@ -66,6 +105,8 @@ class Board:
     def getbit_stone_black(self, n):
         return self.__getbit(self.stone_black, n)
 
+
+    # 石が存在するかどうかを示す変数、または存在する石が黒かどうかを示す変数を更新するためのメソッド
     def setbit_stone_exist(self, n):
         self.stone_exist |= 1 << n
 
@@ -73,36 +114,26 @@ class Board:
         self.stone_black ^= mask
 
 
+    # 空きマスに自身の石を置けるかどうかの真偽値を取得するためのメソッド
     def is_placable(self, startpoint):
-        up, left = divmod(startpoint, self.width)
-        right = self.width - 1 - left
-        down = self.height - 1 - up
+        for step, num in StepNumGenerator(startpoint, self.width, self.height).generator:
+            n_gen = ElementNumRange(startpoint, step, num)
+            try:
+                n = next(n_gen)
+            except StopIteration:
+                continue
 
-        tuples = set()
-        tuples.add((min(left, up), -9))
-        tuples.add((up, -8))
-        tuples.add((min(up, right), -7))
-        tuples.add((left, -1))
-        tuples.add((right, 1))
-        tuples.add((min(down, left), 7))
-        tuples.add((down, 8))
-        tuples.add((min(right, down), 9))
-
-        for limit, step in tuples:
-            flag = False
-            for n in StepGenerator(limit, startpoint, step):
-                if self.getbit_stone_exist(n):
-                    if self.getbit_stone_black(n) ^ self.turn:
-                        flag = True
-                        continue
-                    if flag:
+            if self.getbit_stone_exist(n) and (self.getbit_stone_black(n) ^ self.turn):
+                for n in n_gen:
+                    if self.getbit_stone_exist(n):
+                        if self.getbit_stone_black(n) ^ self.turn:
+                            continue
                         return True
-                    else:
-                        break
-                else:
                     break
+
         return False
 
+    # エージェントが石を置ける箇所の番号をリストで取得するためのメソッド
     def list_placable(self):
         p_list = []
 
@@ -114,3 +145,13 @@ class Board:
                 p_list.append(n)
 
         return p_list
+
+
+
+
+if __name__ == "__main__":
+    board = Board()
+    board.reset()
+
+    # 先手の黒が最初に置けるマス一覧を表示する
+    print(board.list_placable())
