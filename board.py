3a8bd941d6a5d517be02357a77ba6478f87b400e
Tomoto Kazushi <tomoto.kz00@gmail.com>
2022-04-30 15:12:30 +0900

    石数を追加

--- board.py
+++ board.py
@@ -112,6 +112,27 @@ class Board:
     def getbit_stone_black(self, n):
         return self.__getbit("stone_black", n)
 
+    # 1が立っているビットの数を取得
+    def __bits_count(self, x):
+        # 2bitごとの組に分け、立っているビット数を2bitで表現する
+        x = x - ((x >> 1) & 0x5555555555555555)
+
+        # 4bit整数に 上位2bit + 下位2bit を計算した値を入れる
+        x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+
+        x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f # 8bitごと
+        x = x + (x >> 8) # 16bitごと
+        x = x + (x >> 16) # 32bitごと
+        x = x + (x >> 32) # 64bitごと = 全部の合計
+        return x & 0x0000007f
+
+    @property
+    def black_num(self):
+        return self.__bits_count(self.stone_black)
+
+    @property
+    def white_num(self):
+        return self.__bits_count(self.stone_exist) - self.black_num
 
     # 石が存在するかどうかを示す変数、または存在する石が黒かどうかを示す変数を更新するためのメソッド
     def setbit_stone_exist(self, n):
