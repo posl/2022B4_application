0bb4eda245c5e29610fe9bb7da002224db664493
wakamatsu-masahiro <oneteiru@gmail.com>
2022-05-06 11:46:09 +0900

    変数を修正

--- game_tree_reinforce.py
+++ game_tree_reinforce.py
@@ -3,41 +3,43 @@ from board import Board
 import numpy as np
 from random import random
 
+# 動作確認未完了
+
 class GTTDAgent:
     def __init__(self):
-        self.alpha = 0.01
-        self.data_size = Board.action_size
-        self.data_list = np.zeros(self.data_size)
+        self.__alpha = 0.01
+        self.__data_size = Board.action_size
+        self.__data_list = np.zeros(self.__data_size)
 
     def __creat_new_data(self):
-        self.new_data_list = list(map(lambda x : x + np.random.random() - 0.5), self.data_list)
+        self.__new_data_list = list(map(lambda x : x + np.random.random() - 0.5), self.__data_list)
 
     def reset(self):
-        self.data_list = np.zeros(self.data_size)
+        self.__data_list = np.zeros(self.__data_size)
         self.__creat_new_data()
 
-    def set_data(self, data_list):
-        if len(data_list) != self.data_size:
+    def set_data(self, __data_list):
+        if len(__data_list) != self.__data_size:
             return
-        self.data_list = data_list
+        self.__data_list = __data_list
         self.__creat_new_data()
 
     def update(self, reward):
-      self.set_data([(1 - self.alpha) * self.data_list + self.alpha * reward * self.diff_data_list])
+      self.set_data([(1 - self.__alpha) * self.__data_list + self.__alpha * reward * self.diff_data_list])
 
     def get_data(self):
-        return self.data_list  
+        return self.__data_list  
 
     def get_new_data(self):
-        return self.new_data_list
+        return self.__new_data_list
 
 class GTReinforce:
     def __init__(self):
-        self.agent = GTTDAgent()
+        self.__agent = GTTDAgent()
         self.player1 = AlphaBeta(0)
 
     def reset(self):
-        self.agent.reset()
+        self.__agent.reset
 
     def set_player1(self, player):
         self.player1 = player
@@ -47,12 +49,12 @@ class GTReinforce:
         alphabeta = AlphaBeta()
         for i in range(repeat_num):
             board.reset()
-            agent_alphabeta = AlphaBeta(self.agent.get_new_data())
+            agent_alphabeta = AlphaBeta(self.__agent.get_new_data())
             board.set_plan(self.player1, agent_alphabeta, 1)
             #　機械学習用の試合のプログラムを作る
-            # self.agent.update(board.game())
-        return self.agent.get_data()
+            # self.__agent.update(board.game())
+        return self.__agent.get_data()
     
     def makefile(self, filename):
         f = open(filename, "w")
-        f.write(self, self.agent.get_data())
+        f.write(self, self.__agent.get_data())
