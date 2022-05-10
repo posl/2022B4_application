58a5cb6ee06a3d39feed14248af41ed20af8534f
tomoike-masaki <103096775+tomoike-masaki@users.noreply.github.com>
2022-05-10 17:13:21 +0900

    COMの場合自動で石が置かれるようにした

--- design/board.py
+++ design/board.py
@@ -1,3 +1,4 @@
+from functools import cache
 import numpy as np
 from math import ceil
 from contextlib import contextmanager
@@ -37,53 +38,34 @@ class StepNumGenerator:
         yield from self.__gen(-1)
 
 
-# 開始の数とステップ数、要素数を指定する range ジェネレータ
-class ElementNumRange:
-    def __init__(self, startpoint):
-        self.args = [startpoint, None, None]
+# 引数の組み合わせを極力減らした上でキャッシュを利用することによる高速化
+@cache
+def element_num_range(startpoint, step, num):
+    startpoint += step
+    start = startpoint + step
+    stop = startpoint + step * num
 
-    # クラスからのインスタンス生成の回数を減らして、処理速度改善
-    def reset(self, step_num):
-        self.args[1:] = step_num
-        self.count = 0
-        return self
-
-    def __iter__(self):
-        return self
-
-    # getattr() の呼び出しが少なくなるようにして、処理速度改善
-    def __next__(self):
-        startpoint, step, num = self.args
-        count = self.count
-
-        if count >= num:
-            raise StopIteration()
-
-        count += 1
-        self.count = count
-        return startpoint + step * count
+    # 静的型付き言語で書かれているため処理が速い、 range を使うことによる高速化
+    return startpoint, range(start, stop, step)
 
 
-# オセロ盤の特定のマスから全方位探索を行うためのジェネレータ
+# オセロ盤の特定のマスから全方位探索を行うためのイテラブル (再利用は想定していないので、扱い的にはジェネレータと等価)
 class OmniDirectionalSearcher:
     def __init__(self, startpoint):
         self.step_num = StepNumGenerator(startpoint).generator
-        self.range = ElementNumRange(startpoint)
+        self.startpoint = startpoint
 
+    # このメソッドで自身をジェネレータとして返す
     def __iter__(self):
         return self
 
     def __next__(self):
         # この next() の呼び出しで生じる StopIteration 例外をこのジェネレータが生じさせた例外として使う
-        step_num = next(self.step_num)
-        n_gen = self.range.reset(step_num)
+        step, num = next(self.step_num)
 
-        try:
-            n = next(n_gen)
-        except StopIteration:
-            return next(self)
-
-        return n, n_gen
+        if num > 1:
+            return element_num_range(self.startpoint, step, num)
+        return next(self)
 
 
 
@@ -246,6 +228,7 @@ class Board:
         self.player1_plan = player1_plan
         self.player2_plan = player2_plan
 
+    @property
     def plans(self):
         return self.player1_plan, self.player2_plan
 
@@ -310,11 +293,11 @@ class Board:
                 # 引数は pass があったかどうかの真偽値
                 render_func()
 
-    def play(self):
+    def play(self, player1_plan, player2_plan):
         self.reset()
 
-        # start 表示 (コンピュータの設定選択)
-        player1_plan, player2_plan = f()
+        # # start 表示 (コンピュータの設定選択)
+        # player1_plan, player2_plan = f()
 
         self.set_plan(player1_plan, player2_plan)
 
@@ -372,7 +355,7 @@ class Board:
 
 
 
-if __name__ == "__main__" and 0==1:
+if __name__ == "__main__":
     def player(board : Board):
         while 1:
             try:
@@ -395,7 +378,11 @@ if __name__ == "__main__" and 0==1:
     # プレイヤー先行でゲーム開始
     #board.set_plan(player, com_random, 1)
     #board.set_plan(player, player, 1)
-    board.play()
+    board.play(player, com_random)
+
+    print("game set")
+    print("black:", board.black_num)
+    print("white:", board.white_num)
 
 
     #詰み手順の確認
