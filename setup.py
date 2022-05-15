a8be15d82fde1a673d8c4642f946772fe8b0706f
Tomoto Kazushi <tomoto.kz00@gmail.com>
2022-05-15 20:39:08 +0900

    c++に変更

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ file_name = "board_speedup"
 setup(ext_modules = cythonize(file_name + ".pyx"))
 
 # C 言語に変換して、コンパイルする過程でできた作業ファイル、ディレクトリを削除する
-os.remove(file_name + ".c")
+os.remove(file_name + ".cpp")
 shutil.rmtree("build/")
 
 print("\nSuccessfully creating so-file!")
