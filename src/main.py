8410d82dde6b7ce39e0cae2a2b1850d3e0b3c2df
inada tsukasa <invincibility3@icloud.com>
2022-06-09 17:06:22 +0900

    main.py 若干修正

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from argparse import ArgumentParser
+
+from gui_display import DisplayBoard
+
+
+
+parser = ArgumentParser(description = "オセロゲーム")
+parser.add_argument("--ip", default = "0", help = "通信用にサーバを立てた時、その出力である IP アドレスを指定する")
+args = parser.parse_args()
+
+displayboard = DisplayBoard()
+displayboard.play(args.ip)
